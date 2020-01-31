4ad54ac12c08a30e4bec31a509d602b7cda1c1f4
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-01-30 21:09:31 -0500

    created database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,4 @@
-### Schema
-
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS dog_db;
+-- Creates the "blogger" database --
+CREATE DATABASE dog_db;
