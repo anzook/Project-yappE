94c902bb745d1ef3b58116ecb0c5c180bb6d2d8a
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-01 09:27:07 -0500

    edited constraints for id column

--- models/pet.js
+++ models/pet.js
@@ -1,5 +1,11 @@
 module.exports = function (sequelize, DataTypes) {
     const Pet = sequelize.define("pet", {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
+        },
         name: {
             type: DataTypes.STRING,
             allowNull: false
