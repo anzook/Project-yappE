998d6c1e1c00ddd43c1127e38010302373086a1a
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-01 09:27:29 -0500

    edited constraints for id column

--- models/role.js
+++ models/role.js
@@ -1,5 +1,11 @@
 module.exports = function (sequelize, DataTypes) {
     const Role = sequelize.define("role", {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
+        },
         role: {
             type: DataTypes.STRING,
             allowNull: false
