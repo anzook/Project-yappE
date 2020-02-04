d557e41bdd030f4c15362146dba8be4c0df3d401
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-03 19:01:45 -0500

    made foreign key for pet

--- models/action.js
+++ models/action.js
@@ -1,7 +1,24 @@
 module.exports = function (sequelize, DataTypes) {
     const Action = sequelize.define("Action", {
-
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.ENUM,
+            values: ['poop', 'pee', 'high-five', 'sit', 'speak', 'roll-over', 'walked', 'fetch']
+        },
+        type: {
+            type: DataTypes.ENUM,
+            values: ['potty time', 'exercise', 'tricks']
+        }
     });
+
+    Action.associates = function (models) {
+        Action.belongsTo(models.Pet);
+    }
 
     return Action;
 }
