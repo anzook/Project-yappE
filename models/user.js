28498a1490e682da6c4c3f83097ef70b218dfe69
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-04 15:40:22 -0500

    added custom method for password hashing

--- models/user.js
+++ models/user.js
@@ -44,15 +44,26 @@ module.exports = function (sequelize, DataTypes) {
 
     User.associate = function (models) {
         User.belongsToMany(models.Role, {
-            through:'User_Role' // pivot
+            through: 'User_Role' // pivot
         });
 
         User.belongsToMany(models.Pet, {
-            through:'User_Pet' // pivot
+            through: 'User_Pet' // pivot
         });
 
         User.hasMany(models.Action);
     };
+
+    // Creating a custom method for our User model. This will check if an unhashed password entered by 
+    // the user can be compared to the hashed password stored in our database
+    User.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+    // Hooks are automatic methods that run during various phases of the User Model lifecycle
+    // In this case, before a User is created, we will automatically hash their password
+    User.addHook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    });
 
     return User;
 };
