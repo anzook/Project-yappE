c52f1f2de26706a2fcb1e26d192bf788bbcc106c
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-04 16:34:12 -0500

    added route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,7 +4,13 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-
+module.exports = app => {
+    app.get("/", (req, res) => {
+        // If the user already has an account send them to the members page
+        if (req.user) {
+            res.redirect("/test");
+        }
+        res.sendFile(path.join(__dirname, "../templates/signup.html"));
+    });
 
 };
