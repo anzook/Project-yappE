daa6b3615c198977969806d5b8b57869ff42bbc0
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-04 15:56:54 -0500

    Initial commit

---
+++ routes/html-routes.js
@@ -0,0 +1,10 @@
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
+module.exports = function(app) {
+
+
+};
