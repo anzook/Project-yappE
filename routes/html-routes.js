e34a97b2a8d60b434dff1ebef228bcd08e7c0edb
anz213 <56008225+anzook@users.noreply.github.com>
2020-02-04 19:59:49 -0500

    fixed formatting and env

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,30 +1,29 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const path = require('path');
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require('../config/middleware/isAuthenticated');
 
-module.exports = app => {
-    app.get("/", (req, res) => {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect("/test");
-        }
-        res.sendFile(path.join(__dirname, "../templates/signup.html"));
-    });
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect('/test');
+    }
+    res.sendFile(path.join(__dirname, '../templates/signup.html'));
+  });
 
-    app.get("/login", function (req, res) {
-        // If the user already has an account send them to the members page
-        if (req.user) {
-            res.redirect("/members");
-        }
-        res.sendFile(path.join(__dirname, "../public/login.html"));
-    });
+  app.get('/login', function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect('/members');
+    }
+    res.sendFile(path.join(__dirname, '../public/login.html'));
+  });
 
-    // Here we've add our isAuthenticated middleware to this route.
-    // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/test", isAuthenticated, function (req, res) {
-        res.sendFile(path.join(__dirname, "../templates/test.html"));
-    });
-
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get('/test', isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, '../templates/test.html'));
+  });
 };
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 // *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
+// This file is the initial starting point for the Node/Express server.
 //
-// ******************************************************************************
+require('dotenv').config();
+
+// *****************************************************************************
 // *** Dependencies
 // =============================================================
-var express = require("express");
+const express = require('express');
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require('./models');
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Routes
 // =============================================================
@@ -29,6 +31,6 @@ app.use(express.static("public"));
 // =============================================================
 db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log('App listening on PORT ' + PORT);
   });
 });
