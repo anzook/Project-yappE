b5b47e75bfb0542616d46ea6e8c778c704a0201f
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-05 16:11:35 -0500

    removed last name and made the column just 'name'

--- models/user.js
+++ models/user.js
@@ -1,8 +1,7 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
 
 module.exports = function(sequelize, DataTypes) {
-    
   const User = sequelize.define('User', {
     id: {
       type: DataTypes.UUID,
@@ -10,15 +9,7 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
     },
-    first_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      // ====Validations====
-      validate: {
-        len: [1, 30],
-      },
-    },
-    last_name: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
       // ====Validations====
@@ -58,15 +49,19 @@ module.exports = function(sequelize, DataTypes) {
     User.hasMany(models.Action);
   };
 
-  // Creating a custom method for our User model. This will check if an unhashed password entered by
+  // Creating a custom method for our User model. This will check if
+  // an unhashed password entered by
   // the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
+  // Hooks are automatic methods that run during various phases of
+  // the User Model lifecycle
+  // In this case, before a User is created, we will automatically
+  // hash their password
   User.addHook('beforeCreate', function(user) {
-    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    user.password = bcrypt.hashSync(user.password,
+        bcrypt.genSaltSync(10), null);
   });
 
   return User;
