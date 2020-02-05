514b8e0edb105de149d87838c2e76b1f242fe769
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-05 16:12:38 -0500

    fixed syntax to make travis happy

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -10,8 +10,10 @@ module.exports = function(app) {
     res.json(req.user);
   });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // Route for signing up a user. The user's password is
+  // automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user
+  // is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post('/api/signup', function(req, res) {
     db.User.create({
