a3f8523ea538e91f23ee0acc8b0e1668849648e6
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-06 17:08:53 -0500

    adding functions to connect frontend with backend

--- public/js/initial-adds.js
+++ public/js/initial-adds.js
@@ -1,5 +1,4 @@
-/* eslint-disable prefer-const */
-/* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
-/* eslint-disable require-jsdoc */
-$(document).ready(function() {});
+$(document).ready(function() {
+
+});
