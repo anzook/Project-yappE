0e8b78ceb90957c7a2f2a89353b73b1ab8814fad
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-07 10:00:31 -0500

    edited for my server

--- config/passport.js
+++ config/passport.js
@@ -1,37 +1,40 @@
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-const db = require("../models");
+const db = require('../models');
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+// Telling passport we want to use a Local Strategy.
+// In other words, we want login with a username/email and password
 passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
-  {
-    usernameField: "email"
-  },
-  function(email, password, done) {
+    // Our user will sign in using an email, rather than a "username"
+    {
+      usernameField: 'email',
+    },
+    function(email, password, done) {
     // When a user tries to sign in this code runs
-    db.User.findOne({
-      where: {
-        email: email
-      }
-    }).then(function(dbUser) {
+      db.User.findOne({
+        where: {
+          email: email,
+        },
+      }).then(function(dbUser) {
       // If there's no user with the given email
-      if (!dbUser) {
-        return done(null, false, {
-          message: "Incorrect username/password."
-        });
-      }
-      // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
-        return done(null, false, {
-          message: "Incorrect username/password."
-        });
-      }
-      // If none of the above, return the user
-      return done(null, dbUser);
-    });
-  }
+        if (!dbUser) {
+          return done(null, false, {
+            message: 'Incorrect username/password.',
+          });
+        };
+        // If there is a user with the given email,
+        // but the password the user gives us is incorrect
+
+        if (!dbUser.validPassword(password)) {
+          return done(null, false, {
+            message: 'Incorrect username/password.',
+          });
+        }
+        // If none of the above, return the user
+        return done(null, dbUser);
+      });
+    },
 ));
 
 // In order to help keep authentication state across HTTP requests,
