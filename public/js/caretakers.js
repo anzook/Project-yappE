279080942b063c8f7e79fb8bd77ac38a2ce7d657
Phill Mitchell-Matsuyama <phillicia.mitchell21@gmail.com>
2020-02-07 11:48:14 -0500

    reworking code flow

--- public/js/caretakers.js
+++ public/js/caretakers.js
@@ -1,35 +1,35 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable require-jsdoc */
-// $(document).ready(function() {
-// //   const nameInput = $('#caretaker-name');
-//   const roleInput = $('#caretaker-role');
-//   const emailInput = $('#caretaker-email');
-//   const initSubmitbtn = $('#nextBtn');
+$(document).ready(function() {
+  const nameInput = $('#caretaker-name');
+  const roleInput = $('#caretaker-role');
+  const emailInput = $('#caretaker-email');
+  const initSubmitbtn = $('#nextBtn');
 
-//   initSubmitbtn.click(function(event) {
-//     event.preventDefault();
-//     if (initSubmitbtn.html() == 'Submit') {
-//       const caretakerData = {
-//         name: roleInput.val().trim(),
-//         email: emailInput.val().trim(),
-//       };
-//       if (!caretakerData.name || !caretakerData.breed) {
-//         return;
-//       }
-//       addCaretaker(caretakerData.name, caretakerData.email);
-//   });
+  initSubmitbtn.click(function(event) {
+    event.preventDefault();
+    if (initSubmitbtn.html() == 'Submit') {
+      const caretakerData = {
+        name: roleInput.val().trim(),
+        email: emailInput.val().trim(),
+      };
+      if (!caretakerData.name || !caretakerData.breed) {
+        return;
+      }
+      addCaretaker(caretakerData.name, caretakerData.email);
+  });
 
 
-//   function addDog(name, email) {
-//     $.post('/api/caretaker', {
-//       name: name,
-//       email: email,
-//     }) .then(function(data) {
-//       window.location.replace('/homepage');
-//       // If there's an error, handle it by throwing up a bootstrap alert
-//     })
-//         .catch((err)=>{
-//           console.log(err);
-//         });
-//   }
-// });
+  function addDog(name, email) {
+    $.post('/api/caretaker', {
+      name: name,
+      email: email,
+    }) .then(function(data) {
+      window.location.replace('/homepage');
+      // If there's an error, handle it by throwing up a bootstrap alert
+    })
+        .catch((err)=>{
+          console.log(err);
+        });
+  }
+});
