d0460a504755462d80eb234f7a676fa4ae96835a
anz213 <56008225+anzook@users.noreply.github.com>
2020-02-06 20:03:26 -0500

    api js file drafted for images

--- models/pet.js
+++ models/pet.js
@@ -1,34 +1,33 @@
-module.exports = function (sequelize, DataTypes) {
-    const Pet = sequelize.define("Pet", {
-        id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            //====Validations====
-            validate: {
-                len: [1, 30]
-            }
-        }
+module.exports = function(sequelize, DataTypes) {
+  const Pet = sequelize.define('Pet', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // ====Validations====
+      validate: {
+        len: [1, 30],
+      },
+    },
+  });
+
+  Pet.associate = function(models) {
+    Pet.belongsToMany(models.User, {
+      through: 'User_Pet', // pivot
     });
 
-    Pet.associate = function (models) {
-        Pet.belongsToMany(models.User, {
-            through: 'User_Pet' // pivot
-        });
+    Pet.belongsToMany(models.Role, {
+      through: models.User,
+    });
 
-        Pet.belongsToMany(models.Role, {
-            through: models.User
-        });
-
-        // Pet.belongsToMany(models.Contact, {
-        //     through: models.User
-        // });
-
-    };
-    return Pet;
+    // Pet.belongsToMany(models.Contact, {
+    //     through: models.User
+    // });
+  };
+  return Pet;
 };
